cfca3cce807c6b6a7d71942f424cf90cb3f97847
Samuel Flores Carmona <samuelflorescarmona@live.com>
2024-03-17 20:38:42 +0100

    test

--- src/pages/getIgPosts.ts
+++ src/pages/getIgPosts.ts
@@ -1,27 +1,27 @@
-// import fs from 'fs';
-// import type { Post } from '../types/posts';
-// import { getAllIgPostsImages, downloadIgImages, transformImages } from '../utils/igPostsUtils';
-// const rootPath = process.cwd();
-// const igPosts = JSON.parse(fs.readFileSync(`${rootPath}/public/json/igPosts.json`, 'utf-8'));
+import fs from 'fs';
+import type { Post } from '../types/posts';
+import { getAllIgPostsImages, downloadIgImages, transformImages } from '../utils/igPostsUtils';
+const rootPath = process.cwd();
+const igPosts = JSON.parse(fs.readFileSync('../../public/json/igPosts.json', 'utf-8'));
 
-// export async function GET() {
-//   const lastDate = igPosts.lastDate as number;
-//   const currentTimestamp = Date.now();
-//   const diff = (currentTimestamp - lastDate) as number;
-//   const thirtySeconds = 1000 * 30;
-//   const twelveHours = 1000 * 60 * 60 * 12;
-//   if (diff < thirtySeconds || diff > twelveHours || import.meta.env.MODE === 'development') {
-//     const images: Post[] = (await getAllIgPostsImages('')) as Post[];
-//     const transform = await downloadIgImages(images);
-//     if (transform) {
-//       const transformedImages: string[] = await transformImages('', true);
-//       const transformedIgImages: string[] = await transformImages(
-//         `${rootPath}/to_compress/ig`,
-//         false,
-//       );
-//       return new Response(JSON.stringify({ transformedImages, transformedIgImages }));
-//     }
-//   } else {
-//     return new Response('no ha pasao un minuto, surmano');
-//   }
-// }
+export async function GET() {
+  const lastDate = igPosts.lastDate as number;
+  const currentTimestamp = Date.now();
+  const diff = (currentTimestamp - lastDate) as number;
+  const thirtySeconds = 1000 * 30;
+  const twelveHours = 1000 * 60 * 60 * 12;
+  if (diff < thirtySeconds || diff > twelveHours || import.meta.env.MODE === 'development') {
+    const images: Post[] = (await getAllIgPostsImages('')) as Post[];
+    const transform = await downloadIgImages(images);
+    if (transform) {
+      const transformedImages: string[] = await transformImages('', true);
+      const transformedIgImages: string[] = await transformImages(
+        `${rootPath}/to_compress/ig`,
+        false,
+      );
+      return new Response(JSON.stringify({ transformedImages, transformedIgImages }));
+    }
+  } else {
+    return new Response('no ha pasao un minuto, surmano');
+  }
+}
