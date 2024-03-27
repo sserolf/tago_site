1a3a9a9c1efadc903f36690989785a83ae120f69
Samuel Flores Carmona <samuelflorescarmona@live.com>
2024-03-27 16:24:11 +0100

    ig posts url fix and removed unnecessary logic

--- src/utils/igPostsUtils.ts
+++ src/utils/igPostsUtils.ts
@@ -2,37 +2,20 @@ import { turso } from 'src/utils/turso';
 import type { Children, ChildrenData, Post, PostsDbResponse, PostsResponse } from 'src/types/posts';
 import type { AvailableLocales } from 'src/types/language';
 
-export const getAllIgPosts = async (newUrl: string) => {
+export const getAllIgPosts = async () => {
   const access_token: string = import.meta.env.IG_ACCESS_TOKEN as string;
   const url =
     // eslint-disable-next-line max-len
-    'https://graph.facebook.com/v17.0/17841460325620436?fields=business_discovery.username(tagomagoband)%7Bmedia%7Bcaption%2Ctimestamp%2Cpermalink%2Cmedia_type%2Cmedia_url%2Cchildren%7Bmedia_url%2Cmedia_type%7D%7D%7D&access_token=' +
+    'https://graph.facebook.com/v17.0/17841460325620436?fields=business_discovery.username(tagomagoband){media{caption,timestamp,permalink,media_type,media_url,children{media_url,media_type}}}&access_token=' +
     access_token;
   const mediaData: Post[] = [];
-  newUrl = newUrl !== '' ? newUrl : url;
-  const response = await fetch(newUrl, {
-    method: 'GET',
-  });
+  const response = await fetch(url);
   const data = await response.json();
   if (!data.error) {
-    data.business_discovery.media.data.map((media: Post) => {
-      mediaData.push(media);
+    data.business_discovery.media.data.map((post: Post) => {
+      mediaData.push(post);
     });
-    // return mediaData as MediaDatum[];
-    if (data.business_discovery.media.paging.cursors.after) {
-      newUrl =
-        // eslint-disable-next-line max-len
-        'https://graph.facebook.com/v17.0/17841460325620436?fields=business_discovery.username(tagomagoband){media.after(' +
-        data.business_discovery.media.paging.cursors.after +
-        // eslint-disable-next-line max-len
-        '){caption,timestamp,permalink,media_type,media_url,children{media_url,media_type}}}&access_token=' +
-        access_token;
-      const moreData = await getAllIgPosts(newUrl);
-      mediaData.push(...moreData);
-      return mediaData;
-    } else {
-      return mediaData;
-    }
+    return mediaData;
   } else {
     return [];
   }
@@ -164,7 +147,7 @@ const updateLastUpdated = async () => {
 };
 
 export const updateDbPosts = async () => {
-  const igPosts = await getAllIgPosts('');
+  const igPosts = await getAllIgPosts();
   igPosts.map(async (post) => {
     const timestamp = new Date(post.timestamp).getTime().toString();
     const hasChildren =
