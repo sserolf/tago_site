875ff0eb2b7b32287651c2811ed1b946c3b78d9c
Samuel Flores Carmona <samuelflorescarmona@live.com>
2024-03-27 17:31:55 +0100

    test2

--- src/pages/auth.ts
+++ src/pages/auth.ts
@@ -8,11 +8,9 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
   const username = params.get('username');
   const password = params.get('password') as string;
   const hash: string = import.meta.env.PASS;
-  console.log(import.meta.env.PASS);
   const validToken = bcrypt.compareSync(password, hash);
   if (validToken) {
     const token = jwt.sign({ username }, import.meta.env.SECRET);
-    console.log(import.meta.env.SECRET);
     cookies.set('session', token, { maxAge: 60 * 60 });
     return redirect('admin');
   } else {
